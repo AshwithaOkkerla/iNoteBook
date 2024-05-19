ea33a7c9a0e1235a327e9687b5f67ffa14605aad
AshwithaOkkerla <ashwitha1101@gmail.com>
2024-05-19 16:29:01 +0530

    introduced middleware fetchUser which sends user id and included user key in Notes schema as foreign key

--- backendd/routes/auth.js
+++ backendd/routes/auth.js
@@ -2,12 +2,12 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
-var bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
+var bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+var fetchUser = require('../middleware/fetchUser')
+const JWT_SECRET = "ashwithaisagoodgirl";
 
-const JWT_SECRET = "ashwithaisagoodgirl"
-
-//Create a User using : POST '/api/auth/createUser'. no login required
+//Route1 : Create a User using : POST '/api/auth/createUser'. no login required
 router.post(
   "/createUser",
   [
@@ -29,8 +29,8 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with same email exists" });
       }
-	  const salt = await bcrypt.genSaltSync(10);
-	  const securePswd = await bcrypt.hash(req.body.password,salt);
+      const salt = await bcrypt.genSaltSync(10);
+      const securePswd = await bcrypt.hash(req.body.password, salt);
 
       //create a new user
       let user = await User.create({
@@ -38,18 +38,85 @@ router.post(
         password: securePswd,
         email: req.body.email,
       });
-	  const data = {
-		user:{
-			id: user.id
-		}
-	  }
-	 const authToken =  jwt.sign(data,JWT_SECRET)
-	 console.log(authToken)
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      console.log(authToken);
 
-      res.json({ DataEntry: "Successful", authToken:authToken });
+      res.json({ DataEntry: "Successful", authToken: authToken });
     } catch (error) {
       console.log(error.message);
-	  res.status(500).send("Some error occured");
+      res.status(500).send("Some error occured");
+    }
+  }
+);
+
+//Route2: authenticating user using: POST '/api/auth/login'
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "cannot be empty").exists(),
+  ],
+  async (req, res) => {
+    //if there are errors return
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.send({ errors: result.array() });
+    }
+
+    //no errors found in req body
+    const { email, password } = req.body;
+    try {
+      let person = await User.findOne({ email });
+      if (!person) {
+        return res
+          .status(400)
+          .json({ error: "Please login with crct credentials" });
+      }
+      //compare the pswd entered by user and hashstring stored for that particular email , first the User entered pswd is converted to hashstring and then compared
+      const pswdCompare = await bcrypt.compare(password, person.password);
+      if (!pswdCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please login with crct credentials" });
+      }
+      //pswd is matched
+      const data = {
+        user: {
+          id: person.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      //   console.log(authToken);
+
+      res.json({ authToken: authToken });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server error occured");
+    }
+  }
+);
+
+//Route3: Get logged User Details: POST '/api/auth/getUser'.
+router.post(
+  "/getUser",
+	fetchUser,
+  async (req, res) => {
+
+    try {
+      userId = req.user.id;
+	  console.log(userId)
+      const person = await User.findById(userId ).select("-password");
+	//   console.log(person)
+	  res.send(person)
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server error occured");
     }
   }
 );
--- backendd/routes/notes.js
+++ backendd/routes/notes.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const Notes = require("../models/Notes");
 
 const router = express.Router()
+var fetchUser = require('../middleware/fetchUser')
 
-
-router.get('/',(req,res)=>{
-	
-	res.json([])
+//Route 1: Get logged User all notes using : GET 'api/notes/fetchAllNotes
+router.get('/fetchAllNotes',fetchUser,async (req,res)=>{
+	//fetchUser middleware gives us user id which is obtained by req.user.id
+	const notes = await Notes.find({user:req.user.id});
+	res.json(notes);
 })
 
 module.exports = router
