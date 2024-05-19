385113489d463cb5bf71c78a41ecd8d10f231c06
AshwithaOkkerla <ashwitha1101@gmail.com>
2024-05-19 09:49:25 +0530

    initial setup is done

--- backendd/index.js
+++ backendd/index.js
@@ -4,9 +4,11 @@ main()
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+//available routes
+app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/auth', require('./routes/notes'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
