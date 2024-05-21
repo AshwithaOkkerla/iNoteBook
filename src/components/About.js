86bb53063b9f31eb8c22f423eee756adfe65ec93
AshwithaOkkerla <ashwitha1101@gmail.com>
2024-05-21 08:00:27 +0530

    added active function to navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { useContext,useEffect } from 'react'
-import NoteContext from '../context/notes/NoteContext'
+// import { useContext,useEffect } from 'react'
+// import NoteContext from '../context/notes/NoteContext'
 
 const About = () => {
-	const a = useContext(NoteContext)
-	useEffect(()=>{
-		a.update()
-	},[])
+	
   return (
 	<div>
-	This is About {a.state.name} and she is in class {a.state.class}
+	This is About
 	</div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,24 +1,12 @@
 
 import NoteContext from "./NoteContext";
-import { useState } from "react";
+// import { useState } from "react";
 const NoteState = (props) => {
 
-	const s1 = {
-		"name":"Ashwitha",
-		"class":"3"
-	}
-	const [state,setState] = useState(s1);
-	const update = () =>{
-		setTimeout(()=>{
-			setState({
-				"name":"Harry",
-				"class":"Teacher"
-			})
-		},1000);
-	}
+	
   return(
 	 
-	<NoteContext.Provider value={{state,update}}>
+	<NoteContext.Provider value={{}}>
 	{props.children}
 	</NoteContext.Provider>
   )
