768419a364793232b0e05e17be483f0fb105a761
AshwithaOkkerla <ashwitha1101@gmail.com>
2024-05-21 18:08:58 +0530

    updated home.js by addindg usecontext hook to access titles of notes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -10,11 +10,13 @@ function App() {
     <>
       <NoteState>
         <Router>
-          <Navbar />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/about" element={<About />} />
-          </Routes>
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
         </Router>
       </NoteState>
     </>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,15 +1,32 @@
-
+import { useState } from "react";
 import NoteContext from "./NoteContext";
-// import { useState } from "react";
-const NoteState = (props) => {
 
-	
-  return(
-	 
-	<NoteContext.Provider value={{}}>
-	{props.children}
-	</NoteContext.Provider>
-  )
+const NoteState = (props) => {
+  const notesInitial = [
+    {
+      _id: "664c0bce4028113fa00bbd76",
+      user: "664c0b2a4028113fa00bbd72",
+      title: "WebDev-update",
+      description: "doing react and backend projects-update",
+      tag: "personal",
+      date: "2024-05-21T02:49:50.553Z",
+      __v: 0,
+    },
+    {
+      _id: "664c0c3c4028113fa00bbd7a",
+      user: "664c0b2a4028113fa00bbd72",
+      title: "Chilling ",
+      description: "enjoyed summer hldys",
+      tag: "vacation",
+      date: "2024-05-21T02:51:40.026Z",
+      __v: 0,
+    },
+  ];
+
+  	const [notes,setNotes] = useState(notesInitial)
+  return (
+    <NoteContext.Provider value={{notes,setNotes}}>{props.children}</NoteContext.Provider>
+  );
 };
 
 export default NoteState;
