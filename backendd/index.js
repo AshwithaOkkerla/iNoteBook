6c09588f64985e368c83119609a17321c0044c5c
AshwithaOkkerla <ashwitha1101@gmail.com>
2024-05-22 00:04:42 +0530

    added api calls to add and delete:

--- backendd/index.js
+++ backendd/index.js
@@ -1,9 +1,12 @@
 const main = require('./db')
 const express = require('express')
 main()
+var cors = require('cors')
 const app = express()
 const port = 5000
 
+ 
+app.use(cors())
 app.use(express.json())
 //available routes
 app.use('/api/auth', require('./routes/auth'))
